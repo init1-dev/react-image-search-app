d9cf55b44bf89c04640682ea9428123b3ba87b08
init1-dev <init1.dev@gmail.com>
2024-05-13 09:49:24 +0200

    FIX: refactor Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 
 import { RootView } from './views/RootView';
 import { SearchPhotos } from './views/SearchPhotos';
@@ -6,11 +6,28 @@ import { SavedPhotos } from './views/SavedPhotos';
 
 export const appName = '/react-image-search-app';
 
-const appRouter = createBrowserRouter(createRoutesFromElements(
-    <Route path={appName} element={<RootView />}>
-      <Route path={appName} element={<SearchPhotos />} />
-      <Route path={appName + "/saved"} element={<SavedPhotos />} />
-    </Route>
-  ));
+const appRouter = createBrowserRouter([
+    {
+        id: "root",
+        path: appName,
+        Component: RootView,
+        children: [
+            {
+                path: appName,
+                Component: SearchPhotos
+            },
+            {
+                path: appName + "/saved",
+                Component: SavedPhotos
+            },
+            {
+                path: "*",
+                async loader() {
+                    return redirect(appName);
+                }
+            }
+        ]
+    }
+]);
 
 export default appRouter;
